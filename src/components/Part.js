aac2e07122d772e7e157a46278b8152f54cfa8dd
Christopher Babb <chris@appsoftdev.com>
2022-08-02 18:02:08 -0700

    added delete buttons and project complete checkbox

--- src/components/Part.js
+++ src/components/Part.js
@@ -4,14 +4,17 @@ function Part({name, image, price}){
     return(
         <div className='col-sm-12'>
             <div className='row'>
-                <div className='col-sm-3 part-image-container'>
+                <div className='col-sm-2 part-image-container'>
                     <img className='part-image' src={image} alt='part' />
                 </div>
-                <div className='col-sm-6'>
+                <div className='col-sm-4'>
                     <p>{name}</p>
                 </div>
                 <div className='col-sm-3 part-price'>
                     <p>{'$' + price}</p>
+                </div>
+                <div className='col-sm-2'>
+                    <button className="btn btn-primary parts-delete">X</button>
                 </div>
             </div>
         </div>
