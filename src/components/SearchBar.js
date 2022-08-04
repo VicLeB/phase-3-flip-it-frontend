3af38f65538f8200f885bff4d2ab0ad2a2b92af2
Christopher Babb <chris@appsoftdev.com>
2022-08-04 11:13:00 -0700

    Added search functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import {theSearchData} from '../atoms'
 
 function SearchBar(){
+
+    const [searchData, setSearchData] = useRecoilState(theSearchData);
+
     return (
         
         <div className='col-md-4'>
             <form>
                 <div className='form-group house-search'>
-                    <input type='text' className='form-control' placeholder='Search for other Houses' />
+                    <input type='text' className='form-control' placeholder='Search for other Houses' onChange={(e) => setSearchData(e.target.value)} />
                 </div>
             </form>
         </div>
