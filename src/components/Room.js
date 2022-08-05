b09f8d4a5c346e2fce247c03cb5d1c3becd8dc9b
Christopher Babb <chris@appsoftdev.com>
2022-08-04 19:50:38 -0700

    fix some functions when deleting items

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useRecoilState } from 'recoil'
-import {theInitialLoad} from '../atoms'
+import { theRoomInitialLoad } from '../atoms'
 
 function Room({room, handleRoomClick, handleRoomsRender}){
+
+    const [roomInitialLoad, setroomInitialLoad] = useRecoilState(theRoomInitialLoad);
 
     function handleClick(e){
         (e).preventDefault()
@@ -10,12 +12,13 @@ function Room({room, handleRoomClick, handleRoomsRender}){
     }
 
     function handleDeleteClick(){
+        setroomInitialLoad(true)
+        
         fetch(`http://localhost:9292/rooms/${room.id}`, {
             method: "DELETE",
         })
         .then(res => res.json())
-        .then(handleRoomsRender)
-
+        .then(handleRoomsRender) 
     }
 
     return(
